ce352652c36d4c081bec129a6e76fce41eaa40b5
johan-1904 <jbultelle@gmail.com>
2024-12-17 18:09:50 +0100

    Add files via upload

--- c-wire.sh
+++ c-wire.sh
@@ -7,74 +7,112 @@ filtre() {
 	case "$4_$6" in
 	
 	vide_vide)
-		echo "ZIZI"
 		station=$5
-		awk -v stat="$station" 'BEGIN {FS =";"}  $(stat) != "-" {print $(stat), $7, $8}' $1 >> tmps/$fichier.csv
-		sed -i.bak "2d" tmps/$fichier.csv
-		sed -i s/-/0/g tmps/$fichier.csv
+		awk -v stat="$station" 'BEGIN {FS =";"}  $(stat) != "-" {print $(stat), $7, $8}' $1 >> tmp/$fichier.csv
+		sed -i.bak "2d" tmp/$fichier.csv
+		sed -i s/-/0/g tmp/$fichier.csv
 	;;
 	
 	$4_vide)
-		echo "BZEZ"
 		condition=$4
 		station=$5
-		awk  -v cond="$condition" -v stat="$station" 'BEGIN {FS =";"} $stat == cond {print $stat, $7, $8}' $1 >> tmps/$fichier.csv
-		sed -i s/-/0/g tmps/$fichier.csv
+		awk  -v cond="$condition" -v stat="$station" 'BEGIN {FS =";"} $stat == cond {print $stat, $7, $8}' $1 >> tmp/$fichier.csv
+		sed -i s/-/0/g tmp/$fichier.csv
 	;;
 	
 	vide_$6)
-		echo "STRAW"
 		station=$5
 		company=$6
-		awk -v stat="$station" -v comp="$company" 'BEGIN {FS =";"} $(stat) != "-" && $(comp) != "-" {print $stat, $7, $8}' $1 >> tmps/$fichier.csv
-		sed -i.bak "2d" tmps/$fichier.csv
-		sed -i s/-/0/g tmps/$fichier.csv
+		awk -v stat="$station" -v comp="$company" 'BEGIN {FS =";"} $(stat) != "-" && $(comp) != "-" {print $stat, $7, $8}' $1 >> tmp/$fichier.csv
+		sed -i.bak "2d" tmp/$fichier.csv
+		sed -i s/-/0/g tmp/$fichier.csv
 	;;
 	
 	*)
-		echo "PENIS"
 		condition=$4
 		station=$5
 		company=$6
-		awk -v stat="$station" -v cond="$condition" -v comp="$company" 'BEGIN {FS =";"} $stat == cond && ($(comp) != "-" || $7 != "-") {print $stat, $7, $8}' $1 >> tmps/$fichier.csv
-		sed -i s/-/0/g tmps/$fichier.csv
+		awk -v stat="$station" -v cond="$condition" -v comp="$company" 'BEGIN {FS =";"} $stat == cond && ($(comp) != "-" || $7 != "-") {print $stat, $7, $8}' $1 >> tmp/$fichier.csv
+		sed -i s/-/0/g tmp/$fichier.csv
 	;;
 	esac
 }
 
-check_case()			
+vérifier_arg4(){
+	
+	condition=$3
+	
+	if [ $2 = hvb ]
+	then
+		if ! awk -v cond="$condition" 'BEGIN {FS =";"} {if ($2 == cond) {found=1; exit}} END {if(!found) exit 1}' $1
+		then 
+			echo "L'identifiant de centrale que vous avez choisi ne correspond à aucune centrale hvb.
+Temps d'exécution du programme : 0.0 secondes."
+			exit 1
+		fi
+	fi
+	
+	if [ $2 = hva ]
+	then
+		if ! awk -v cond="$condition" 'BEGIN {FS =";"} {if ($3 == cond) {found=1; exit}} END {if(!found) exit 1}' $1
+		then 
+			echo "L'identifiant de centrale que vous avez choisi ne correspond à aucune centrale hva.
+Temps d'exécution du programme : 0.0 secondes."
+			exit 1
+		fi
+	fi
+	
+	if [ $2 = lv ]
+	then
+		if ! awk -v cond="$condition" 'BEGIN {FS =";"} {if ($4 == cond) {found=1; exit}} END {if(!found) exit 1}' $1
+		then 
+			echo "L'identifiant de centrale que vous avez choisi ne correspond à aucune centrale lv.
+Temps d'exécution du programme : 0.0 secondes."
+			exit 1
+		fi
+	fi
+}
+
+vérifier_cas() {			
 		case "$2_$3" in
 
 		hvb_comp)
-			echo "Identifiant Capacité Consommation" > tmps/$fichier.csv
+			echo "Identifiant Capacité Consommation" > tmp/$fichier.csv
 			filtre "$1" "$2" "$3" "$4" 2 vide 
-			cat tmps/$fichier.csv
+			cat tmp/$fichier.csv
 		;;
 	
 		hva_comp)
-			echo "Identifiant Capacité Consommation" > tmps/$fichier.csv
+			echo "Identifiant Capacité Consommation" > tmp/$fichier.csv
 			filtre "$1" "$2" "$3" "$4" 3 vide
-			cat tmps/$fichier.csv
+			cat tmp/$fichier.csv
 		;;
 	
 		lv_comp)
 		
-			echo "Identifiant Capacité Consommation" > tmps/$fichier.csv
+			echo "Identifiant Capacité Consommation" > tmp/$fichier.csv
 			filtre "$1" "$2" "$3" "$4" 4 5
-			cat tmps/$fichier.csv
+			cat tmp/$fichier.csv
 	
 		;;
 	
 		lv_all) 
-			echo "Identifiant Capacité Consommation" > tmps/$fichier.csv
+			echo "Identifiant Capacité Consommation" > tmp/$fichier.csv
 			filtre "$1" "$2" "$3" "$4" 4 vide
-			cat tmps/$fichier.csv
+			cat tmp/$fichier.csv
+			
+			touch tmp/lv_all_minmax.csv
+			sort -n -r -t " " -k 3 tmp/$fichier.csv > tmp/tmp.csv
+			tail -n 1 tmp/tmp.csv > tmp/final.csv
+			head -n -1 tmp/tmp.csv > tmp/tri.csv
+			head -n 10 tmp/tri.csv >> tmp/lv_all_minmax.csv
+			tail -n 10 tmp/tri.csv >> tmp/lv_all_minmax.csv
 		;;
 	
 		lv_indiv)
-			echo "Identifiant Capacité Consommation" > tmps/$fichier.csv
+			echo "Identifiant Capacité Consommation" > tmp/$fichier.csv
 			filtre "$1" "$2" "$3" "$4" 4 6
-			cat tmps/$fichier.csv
+			cat tmp/$fichier.csv
 		;;
 	
 		*) 	
@@ -82,28 +120,44 @@ check_case()
 Il y a une erreur avec les arguments !"
 			if [ $4 = vide ]	
 			then
-				rm tmps/$2_$3.csv
+				rm tmp/$2_$3.csv
 			else
-				rm tmps/$2_$3_$4.csv
+				rm tmp/$2_$3_$4.csv
 			fi
 			aide
 		;;  
 	
 		esac
 
-	
+}	
 
 aide() {
 	echo "				
-Voici une aide pour vous assister :
+Le nombre d'arguments que vous placez en paramètres doit être de 3 ou 4 avec les arguments suivants : 
 
-Premier argument (obligatoire) = Chemin vers le fichier
-Deuxième argument (obligatoire) = 'hvb', 'hva' ou 'lv'
-Troisième argument (obligatoire) = 'all', 'indiv' ou 'comp'
-Quatrième argument (optionnel) = L'identifiant d'une centrale
+Premier argument : 
+		• Obligatoire 
+		-> Il doit être le chemin vers le fichier que vous voulez traiter.
 
-De plus, les combinaisons 'hvb_all', 'hvb_indiv', 'hva_all' et 'hva_indiv' ne sont pas autorisées.
-"
+Deuxième argument :
+		• Obligatoire 
+		-> Il doit correspondre au type de station que vous voulez traiter. Ici 'hvb', 'hva' ou 'lv'.
+
+Troisième argument 
+		• Obligatoire  
+		-> Il doit correspondre au type de consommateurs que vous voulez traiter. 
+			-> Pour sélectionner tous les consommateurs : 'all'. 
+			-> Pour sélectionner uniquement les particuliers : 'indiv'.
+			-> Pour sélectionner uniquement les entreprises : 'comp'.
+
+Quatrième argument
+		• Optionnel
+		-> Si vous le souhaitez, vous pouvez entrer l'identifiant d'une centrale pour traiter uniquement celle-ci.
+
+ATTENTION !!! 
+Les combinaisons 'hvb_all', 'hvb_indiv', 'hva_all' et 'hva_indiv' ne sont pas autorisées.
+
+Temps d'execution du programme : 0.0 secondes."
 		exit 2
 }
 
@@ -130,15 +184,15 @@ mauvais nombre d'arguments"
 	
 fi
 
-if [ -d "tmps" ]
+if [ -d "tmp" ]
 
 then
 
-	rm -r tmps
+	rm -r tmp
 	
 fi
 
-mkdir tmps
+mkdir tmp
 
 if [  ! -d "graphs" ]
 
@@ -164,11 +218,12 @@ Impossible de lire le fichier"
 	
 fi
 
-if [ ! -e exec ]
+if [ ! -e CodeC/exec ]
 
 then
-
-	gcc codeC/exo.c -o exec
+	echo "Erreur, le code en c ne s'est pas compilé correctement !
+Temps d'exécution du programme : 0.0 secondes."
+	exit 4
 	
 fi
 
@@ -187,24 +242,23 @@ if [ $# -eq 4 ]
 then 
 	if [ "$4" -eq "$4" ] 2>/dev/null
 	then
-		
-		echo "CACA"
+		vérifier_arg4 "$1" "$2" "$4"
 		fichier="$2_$3_$4"	
-		touch tmps/$fichier.csv
-		check_case "$1" "$2" "$3" "$4"
+		touch tmp/$fichier.csv
+		vérifier_cas "$1" "$2" "$3" "$4"
 	else
-		echo "PROUT"
 		fichier="$2_$3"	
-		touch tmps/$fichier.csv
-		check_case "$1" "$2" "$3" vide
+		touch tmp/$fichier.csv
+		vérifier_cas "$1" "$2" "$3" vide
 	fi
 else
-	echo "CAKE"
 	fichier="$2_$3"	
-	touch tmps/$fichier.csv
-	check_case "$1" "$2" "$3" vide
+	touch tmp/$fichier.csv
+	vérifier_cas "$1" "$2" "$3" vide
 fi
 
 fin=$(date +%s)
 duree=$(( $fin - $debut ))
-echo "$debut $fin $duree secondes"
+echo "Temps d'exécution du programme : $duree secondes."
+
+
