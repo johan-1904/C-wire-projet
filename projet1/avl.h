a8bc12bb58f31d63474ed8050f5fe42294d274fa
ismael aqouar <skyronebro35@gmail.com>
2024-12-19 12:13:32 +0100

    Add files via upload

---
+++ projet1/avl.h
@@ -0,0 +1,27 @@
+#ifndef AVL_H
+#define AVL_H
+#include "station.h"
+
+typedef struct avl_struct {
+    Station station;
+    int eq;  // Équilibre de l'AVL
+    struct avl_struct *fg;
+    struct avl_struct *fd;
+} AVL;
+
+AVL* creerAVL(Station station);
+int hauteur(AVL* a);
+AVL* rotationDroite(AVL* a);
+AVL* rotationGauche(AVL* a);
+AVL* doubleRotationGauche(AVL* a);
+AVL* doubleRotationDroite(AVL* a);
+AVL* equilibrerAVL(AVL* a);
+AVL* insert(AVL* a, Station new_station);
+void ecrire_infixe(AVL* a, FILE* fichier);
+void liberer(AVL* a);
+int min3(int a, int b, int c);
+int max3(int a, int b, int c);
+int min(int a, int b);
+int max(int a, int b);
+
+#endif
