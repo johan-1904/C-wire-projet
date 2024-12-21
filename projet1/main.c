be088fc1b0de9456eab690c5674db4b57db47d2f
ismael aqouar <skyronebro35@gmail.com>
2024-12-21 12:17:10 +0100

    Update main.c

--- projet1/main.c
+++ projet1/main.c
@@ -19,18 +19,20 @@ int est_nombre_valide(const char* str) { // verifie si la cahine n'est pas nulle
 }
 
 
-void traitement_fichier(FILE* fichier, AVL** a) {
+int traitement_fichier(FILE* fichier, AVL** a) {
+    
     char id_str[100], capacite_str[100], consommation_str[100], ligne[100];
+    
     while (fgets(ligne, sizeof(ligne), fichier) != NULL) { // boucle qui lit chaque ligne du fichier 
         ligne[strcspn(ligne, "\n")] = '\0'; // supprime les \n en fin de ligne 
         Station station;
         int n = sscanf(ligne, "%s %s %s", id_str, capacite_str, consommation_str); //decoupe chaque ligne en trois parties
         if (n != 3) { // verifie si les trois parties sont la 
-            exit(1);
+            return 1;
         }
 
         if (!est_nombre_valide(id_str) || !est_nombre_valide(capacite_str) || !est_nombre_valide(consommation_str)) { // verifie si les 3 parties sont des nombres valides
-            exit(1);
+            return 1;
         }
         station.id = strtol(id_str, NULL, 10); // convertit en entier long 
         station.capacite = strtol(capacite_str, NULL, 10);
@@ -38,7 +40,7 @@ void traitement_fichier(FILE* fichier, AVL** a) {
 
         *a = insert(*a, station);
     }
-
+    return 0;
 }
 
 
@@ -46,18 +48,20 @@ int main(int argc, char** argv) {
     
     if (argc < 2) { // verifie les arguments 
         printf( "Erreur d'argument\n");
-        exit(1);
+        return 1;
     }
     
     FILE* fichier = fopen(argv[1], "r+"); // ouvre le fichier
     if (fichier == NULL) { // verifie si le fichier est bien ouvert
         printf("Erreur avec le fichier\n");
-        exit(1);
+        return 1;
     }
     
 
     AVL* a = NULL;
-    traitement_fichier(fichier,&a); //lit le fichier et remplit l'avl
+    if (traitement_fichier(fichier,&a)){//lit le fichier et remplit l'avl
+    	return 1;
+    }
     freopen(argv[1], "w", fichier); // réouvre le fichier et ecrase son contenu
     ecrire_infixe(a, fichier);
     liberer(a); // libere la memoire prise par l'avl
